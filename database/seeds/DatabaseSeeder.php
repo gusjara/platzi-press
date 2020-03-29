c850f8f10f6fdf22a0f025c66ed428efd2c0ffb1
gusjara <tavit02@hotmail.com>
2020-03-29 02:10:36 -0300

    init commit, with some fuctions until posts index.

---
+++ database/seeds/DatabaseSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // $this->call(UsersTableSeeder::class);
+
+
+        App\User::create([
+        	'name' => 'Gus',
+        	'email' => 'admin@press.link',
+        	'password' => bcrypt('654321')
+        ]);
+
+        factory(App\Post::class, 24)->create();
+    }
+}
---
+++ resources/views/posts/index.blade.php
@@ -0,0 +1,56 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">
+                    Articles <a class="btn btn-sm btn-primary float-right" href="{{ route('posts.create') }}">Create</a>
+                </div>
+
+                <div class="card-body">
+                    @if (session('status'))
+                        <div class="alert alert-success" role="alert">
+                            {{ session('status') }}
+                        </div>
+                    @endif
+
+                    <table class="table">
+                        <thead>
+                            <th>ID</th>
+                            <th>Title</th>
+                            <th colspan="2">&nbsp;</th>
+                        </thead>
+                        <tbody>
+                            @foreach($posts as $post)
+                            <tr>
+                                <td>{{ $post->id}}</td>
+                                <td>{{ $post->title}}</td>
+                                <td>
+                                    <a class="btn btn-sm btn-secondary" href="{{ route('posts.edit', $post) }}">
+                                        Edit
+                                    </a>
+                                </td>
+                                <td>
+                                    <form action="{{ route('posts.destroy', $post) }}" method="POST">
+                                        @csrf
+                                        @method('DELETE')
+                                        <input  
+                                            type="submit"
+                                            class="btn btn-sm btn-danger" 
+                                            value="Delete"
+                                            onclick="return confirm('Are you sure you want to delete?')" 
+                                        >
+                                    </form>
+                                </td>
+                            </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
